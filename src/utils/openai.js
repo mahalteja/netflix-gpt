b56705262473df184d2adfc0e4e4be5321e2bd5c
mahal teja <mahaltejapilla08@gmail.com>
2025-06-29 13:08:30 +0530

    Open Ai Key removed

--- src/utils/openai.js
+++
@@ -1,8 +0,0 @@
-import OpenAI from "openai";
-// console.log(import.meta.env.OPENAI_API_KEY)
-const openai = new OpenAI({
-  apiKey:"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",
-  dangerouslyAllowBrowser: true,
-});
-
-export default openai;
